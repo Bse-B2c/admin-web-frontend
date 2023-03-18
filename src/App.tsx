7c5e42bf7fdbbbf78b177ba51988e3fcf3f6b2ae
Rodrigo Limões <rolimoes@gmail.com>
2023-03-18 16:09:27 -0300

    Adding ThemeProvider; Adding palette in theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import Layout from '@layouts/Layout';
-import Grid from '@mui/material/Grid';
+import { createTheme, responsiveFontSizes, ThemeProvider } from '@mui/material';
+import { grey, purple } from '@mui/material/colors';
+
+let theme = createTheme({
+	palette: {
+		primary: {
+			main: purple[500],
+		},
+		secondary: {
+			main: grey[500],
+		},
+		background: {
+			default: grey[100],
+		},
+	},
+});
+theme = responsiveFontSizes(theme);
 
 function App() {
 	return (
-		<Grid container>
+		<ThemeProvider theme={theme}>
 			<Layout />
-		</Grid>
+		</ThemeProvider>
 	);
 }
 
