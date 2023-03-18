e49b5b51e9c714a267430aaee562c1c62c44f917
Rodrigo Limões <rolimoes@gmail.com>
2023-03-18 13:49:42 -0300

    Adding Components path

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import * as path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@layouts": path.resolve(__dirname, "./src/layouts"),
-    },
-  },
-  plugins: [react()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+			'@layouts': path.resolve(__dirname, './src/layouts'),
+			'@components': path.resolve(__dirname, './src/components'),
+		},
+	},
+	plugins: [react()],
 });
