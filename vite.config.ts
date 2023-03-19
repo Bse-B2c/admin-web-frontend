34bbeef838bde2812188b1d46988f551ea12c40d
Rodrigo Limões <rolimoes@gmail.com>
2023-03-18 21:31:09 -0300

    Adding pages, and features path

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,8 @@ export default defineConfig({
 			'@': path.resolve(__dirname, './src'),
 			'@layouts': path.resolve(__dirname, './src/layouts'),
 			'@components': path.resolve(__dirname, './src/components'),
+			'@pages': path.resolve(__dirname, './src/pages'),
+			'@features': path.resolve(__dirname, './src/features'),
 			'@store': path.resolve(__dirname, './src/store'),
 		},
 	},
