986589ab7774f9daf429e8b2751e36ebf223b12d
Rodrigo Limões <rolimoes@gmail.com>
2023-03-21 23:41:05 -0300

    Adding authApi reducer

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import appBarReducer from '@store/appBar/appBarSlice';
+import { authApi } from '@features/authentication';
 
 export const store = configureStore({
 	reducer: {
+		[authApi.reducerPath]: authApi.reducer,
 		appBar: appBarReducer,
 	},
+	devTools: true,
+	middleware: getDefaultMiddleware =>
+		getDefaultMiddleware().concat([authApi.middleware]),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
