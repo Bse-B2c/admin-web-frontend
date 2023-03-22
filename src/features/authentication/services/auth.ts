f8727d09deb35f3df603ce5f4b57fbfeed9e9140
Rodrigo Limões <rolimoes@gmail.com>
2023-03-22 10:34:56 -0300

    Adding getMe query

--- src/features/authentication/services/auth.ts
+++ src/features/authentication/services/auth.ts
@@ -1,25 +1,30 @@
 import { apiSlice } from '@/lib/apiSlice';
 import { getServiceHost } from '@utils/getServiceHost';
 import type { Tokens } from '@features/authentication';
+import { User } from '@/model/User';
 import { ApiResponse } from '@/model/ApiResponse';
 
 const service = getServiceHost('account');
 
-export const authApi = apiSlice(`${service}/api/account/auth/`).injectEndpoints(
-	{
-		endpoints: builder => ({
-			login: builder.mutation<Tokens, any>({
-				query: body => ({
-					url: 'admin/signin',
-					method: 'POST',
-					body,
-				}),
-				transformResponse: (response: ApiResponse<Tokens>) => {
-					return response.data;
-				},
+export const authApi = apiSlice(`${service}/api/account/`).injectEndpoints({
+	endpoints: builder => ({
+		login: builder.mutation<Tokens, any>({
+			query: body => ({
+				url: 'auth/admin/signin',
+				method: 'POST',
+				body,
 			}),
+			transformResponse: (response: ApiResponse<Tokens>) => {
+				return response.data;
+			},
 		}),
-	}
-);
+		getMe: builder.query<User, void>({
+			query: () => `user/me`,
+			transformResponse: (response: ApiResponse<User>) => {
+				return response.data;
+			},
+		}),
+	}),
+});
 
-export const { useLoginMutation } = authApi;
+export const { useLoginMutation, useGetMeQuery } = authApi;
