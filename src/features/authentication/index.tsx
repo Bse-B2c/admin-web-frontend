1c96ccaa706945f998739ff37872f3dc4c338b11
Rodrigo Limões <rolimoes@gmail.com>
2023-03-23 09:56:26 -0300

    Exporting useAuthUser hooks

--- src/features/authentication/index.tsx
+++ src/features/authentication/index.tsx
@@ -8,12 +8,14 @@ import {
 } from '@features/authentication/utils/utilsAuth';
 import { Tokens } from '@features/authentication/model/Tokens';
 import { authApi } from '@features/authentication/services/auth';
+import { useAuthUser } from '@features/authentication/hooks/useAuthUser';
 
 export type { Tokens };
 export {
 	LoginForm,
 	RequireAuth,
 	authApi,
+	useAuthUser,
 	getToken,
 	getRefreshToken,
 	removeTokens,
