1cd23aa61e343a188618be2e55e57f2b5cb825ea
Rodrigo Limões <rolimoes@gmail.com>
2023-04-02 20:13:31 -0300

    feat: Adding Customer Item table

---
+++ src/features/customer/components/customers/CustomerItems.tsx
@@ -0,0 +1,60 @@
+import { Delete, Edit, MoreHoriz, Visibility } from '@mui/icons-material';
+import React from 'react';
+import { User } from '@/model/User';
+import { formatDate } from '@utils/utilsDate';
+import MenuButton from '@components/MenuButton';
+
+export const fields = [
+	{
+		label: 'Name',
+		key: 'name',
+	},
+	{
+		label: 'E-mail',
+		key: 'email',
+	},
+	{
+		label: 'Phone',
+		key: 'phone',
+	},
+	{
+		label: 'Cpf',
+		key: 'cpf',
+	},
+	{
+		label: 'Create At',
+		key: 'createAt',
+	},
+	{
+		label: '',
+		key: 'options',
+	},
+];
+
+export const getCustomerItem = () => ({
+	name: ({ name }: User) => {
+		return <div>{name}</div>;
+	},
+	email: ({ email }: User) => <div>{email}</div>,
+	phone: ({ phone }: User) => <div>{phone}</div>,
+	cpf: ({ cpf }: User) => <div>{cpf}</div>,
+	createAt: ({ createdAt }: User) => (
+		<div>{formatDate(createdAt, 'en-US')}</div>
+	),
+	options: () => (
+		<div>
+			<MenuButton
+				options={[
+					{ label: 'Delete', onClick: () => {}, icon: <Delete /> },
+					{
+						label: 'Edit',
+						onClick: () => {},
+						icon: <Edit />,
+					},
+					{ label: 'View', onClick: () => {}, icon: <Visibility /> },
+				]}>
+				<MoreHoriz />
+			</MenuButton>
+		</div>
+	),
+});
