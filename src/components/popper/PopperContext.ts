8da0d6dc82fe0c0343179fe377f8c3e0b2766138
Rodrigo Limões <rolimoes@gmail.com>
2023-04-02 12:16:31 -0300

    feat: Adding Popper context

---
+++ src/components/popper/PopperContext.ts
@@ -0,0 +1,20 @@
+import { createContext, MouseEvent, useContext } from 'react';
+
+interface Context {
+	handleClick: (event: MouseEvent<HTMLElement>) => void;
+	handleClose: () => void;
+	anchorEl: null | HTMLElement;
+}
+
+export const PopperContext = createContext<Context | null>(null);
+
+export const usePopperContext = () => {
+	let context = useContext(PopperContext);
+
+	if (!context)
+		throw new Error(
+			'Child components of Popper cannot be rendered outside the Card component!'
+		);
+
+	return context;
+};
