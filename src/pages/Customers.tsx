fa0d5ccf3aabf25bef19caa16b7d9e14495b1a68
Rodrigo Limões <rolimoes@gmail.com>
2023-04-02 20:13:53 -0300

    feat: Adding Customers Page

--- src/pages/Customers.tsx
+++ src/pages/Customers.tsx
@@ -1,4 +1,8 @@
 import React, { FC } from 'react';
+import { Button } from '@mui/material';
+import { Add } from '@mui/icons-material';
+import CustomersList from '@features/customer/components/customers/CustomerList';
+import ContentHeader from '@components/contentHeader/ContentHeader';
 
 interface CustomersStateProps {}
 interface CustomersDispatchProps {}
@@ -6,7 +10,21 @@ interface CustomersDispatchProps {}
 type CustomersProps = CustomersStateProps & CustomersDispatchProps;
 
 const Customers: FC<CustomersProps> = () => {
-	return <div />;
+	return (
+		<ContentHeader
+			title="Customers"
+			endAdornment={
+				<Button
+					color={'success'}
+					size={'small'}
+					variant="outlined"
+					startIcon={<Add />}>
+					New Customer
+				</Button>
+			}>
+			<CustomersList />
+		</ContentHeader>
+	);
 };
 
 export default Customers;
