c1a1a3071213b2a1644536d99bad343caa2089f0
Rodrigo Limões <rolimoes@gmail.com>
2023-04-08 15:53:51 -0300

    feat: Adding Address Modal

---
+++ src/features/customer/components/dataManagement/AddressModal.tsx
@@ -0,0 +1,46 @@
+import React, { FC } from 'react';
+import { Modal } from '@components/modal';
+import { Button, Grid } from '@mui/material';
+import { Close, Save } from '@mui/icons-material';
+
+interface AddressModalStateProps {
+	open: boolean;
+}
+interface AddressModalDispatchProps {
+	onClose: () => void;
+}
+
+type AddressModalProps = AddressModalStateProps & AddressModalDispatchProps;
+
+//TODO: Add Address Form
+//TODO: Add onClick event in Create button
+const AddressModal: FC<AddressModalProps> = ({ open, onClose }) => {
+	return (
+		<Modal open={open} alignment={'center'} buttonClose onClose={onClose}>
+			<Modal.Header>Add Address</Modal.Header>
+			<Modal.Content>Form</Modal.Content>
+			<Modal.Footer>
+				<Grid container justifyContent="end">
+					<Button
+						size="small"
+						color="secondary"
+						variant="contained"
+						startIcon={<Close />}
+						sx={{ marginRight: 1 }}
+						onClick={onClose}>
+						Close
+					</Button>
+					<Button
+						size="small"
+						color="success"
+						variant="contained"
+						startIcon={<Save />}>
+						Create
+					</Button>
+				</Grid>
+			</Modal.Footer>
+		</Modal>
+	);
+};
+
+export default AddressModal;
