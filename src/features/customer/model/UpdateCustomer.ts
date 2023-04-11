b7689a3c57b7a3a65790507fa38476f921ef2aa4
Rodrigo Limões <rolimoes@gmail.com>
2023-04-11 10:02:04 -0300

    refactor: Adding getCustomer query; Adding UpdateCustomer mutation

---
+++ src/features/customer/model/UpdateCustomer.ts
@@ -0,0 +1,8 @@
+export interface UpdateCustomer {
+	id: number;
+	name: string;
+	email: string;
+	phone: string;
+	cpf: string;
+	brithDate: string;
+}
--- src/features/customer/services/customerApi.ts
+++ src/features/customer/services/customerApi.ts
@@ -4,6 +4,7 @@ import { User } from '@/model/User';
 import { ApiResponse } from '@/model/ApiResponse';
 import { createApi } from '@reduxjs/toolkit/query/react';
 import { SearchCustomer } from '@features/customer/model/searchCustomer';
+import { UpdateCustomer } from '@features/customer/model/UpdateCustomer';
 
 const service = getServiceHost('account');
 
@@ -16,7 +17,22 @@ export const customerApi = createApi({
 				`user?name=${name}&startDate=${startDate}&endDate=${endDate}&orderBy=${orderBy}&sortOrder=${sortOrder}&page=${page}&limit=${limit}`,
 			transformResponse: (response: ApiResponse<Array<User>>) => response.data,
 		}),
+		getCustomer: builder.query<User, string>({
+			query: id => `user/${id}`,
+			transformResponse: (response: ApiResponse<User>) => response.data,
+		}),
+		updateCustomer: builder.mutation<ApiResponse<User>, UpdateCustomer>({
+			query: ({ id, ...body }) => ({
+				url: `/user/${id}`,
+				method: 'PATCH',
+				body,
+			}),
+		}),
 	}),
 });
 
-export const { useSearchCustomerQuery } = customerApi;
+export const {
+	useSearchCustomerQuery,
+	useLazyGetCustomerQuery,
+	useUpdateCustomerMutation,
+} = customerApi;
