fc0d4ca9543549a23bd9d47d8f3041da62888959
Rodrigo Limões <rolimoes@gmail.com>
2023-04-12 09:44:00 -0300

    feat: Adding Notification component

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { lazy } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import { store } from '@/store';
+const Notification = lazy(
+	() => import('@components/notification/Notification')
+);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
 		<Provider store={store}>
 			<BrowserRouter>
+				<Notification />
 				<App />
 			</BrowserRouter>
 		</Provider>
